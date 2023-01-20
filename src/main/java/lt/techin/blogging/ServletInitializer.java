f0c3186f8041525a7f8d60084abc8962945429ee
sandra jonikaite <sandrajonikaite@gmail.com>
2023-01-20 09:34:24 +0200

    created model

---
+++ src/main/java/lt/techin/blogging/ServletInitializer.java
@@ -0,0 +1,13 @@
+package lt.techin.blogging;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BloggingApplication.class);
+	}
+
+}
---
+++ src/main/java/lt/techin/blogging/model/Record.java
@@ -0,0 +1,32 @@
+package lt.techin.blogging.model;
+
+public class Record {
+
+    private Long id;
+    private String name;
+    private String text;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
